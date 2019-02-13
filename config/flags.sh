4122e478830e144d81012d6dfa3ad52b22e0571a
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2019-02-13 15:56:45 +0100

    typo

--- config/flags.sh
+++ config/flags.sh
@@ -2,8 +2,8 @@
 export PKG_CONFIG_PATH="$(opam config var lib)/pkgconfig"
 freestanding_ldflags="$(pkg-config gmp-freestanding --libs)"
 freestanding_cflags="$(pkg-config gmp-freestanding ocaml-freestanding --cflags)"
-xen_ldflags="$(pkg-config mirage-xen-posix gmp-xen --cflags) -O2 -pedantic -fomit-frame-pointer -fno-builtin"
-xen_cflags="$(pkg-config gmp-xen --cflags)"
+xen_cflags="$(pkg-config mirage-xen-posix gmp-xen --cflags)-O2 -pedantic -fomit-frame-pointer -fno-builtin"
+xen_ldflags="$(pkg-config gmp-xen --libs)"
 echo "$freestanding_ldflags" > ldflags_freestanding
 echo "$freestanding_cflags" > cflags_freestanding
 echo "$xen_ldflags" > ldflags_xen
