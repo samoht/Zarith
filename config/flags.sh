8980099189e40635f11c3395d7014a9507c5a691
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2019-03-21 16:52:02 +0100

    Lil' mistake

--- config/flags.sh
+++ config/flags.sh
@@ -2,7 +2,7 @@
 export PKG_CONFIG_PATH="$(opam config var lib)/pkgconfig"
 freestanding_ldflags="$(pkg-config gmp-freestanding --libs)"
 freestanding_cflags="$(pkg-config gmp-freestanding ocaml-freestanding --cflags)"
-xen_cflags="$(pkg-config mirage-xen-posix gmp-xen --cflags)-O2 -pedantic -fomit-frame-pointer -fno-builtin"
+xen_cflags="$(pkg-config mirage-xen-posix gmp-xen --cflags) -O2 -pedantic -fomit-frame-pointer -fno-builtin"
 xen_ldflags="$(pkg-config gmp-xen --libs)"
 echo "$freestanding_ldflags" > ldflags_freestanding
 echo "$freestanding_cflags" > cflags_freestanding
